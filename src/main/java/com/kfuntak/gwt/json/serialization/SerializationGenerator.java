6fb0902c13c39478934c6e3c5d18763850529f71
google@alishomepage.com <google@alishomepage.com@ae6a6801-496e-cf2b-c46a-549bbc5cf0b7>
2010-12-12 15:18:12 +0000

    cleanup

--- src/main/java/com/kfuntak/gwt/json/serialization/SerializationGenerator.java
+++ src/main/java/com/kfuntak/gwt/json/serialization/SerializationGenerator.java
@@ -2,7 +2,6 @@ package com.kfuntak.gwt.json.serialization;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
